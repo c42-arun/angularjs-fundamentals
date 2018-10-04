b07d5961b8e749a8e134ff4f96c8df1ccd83fa87
Arunkumar Ramachandriya <arunkumar@hawesandcurtis.com>
2018-10-04 09:20:12 +0100

    event handling with actual data passed across

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -18,23 +18,34 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob Smith',
                     duration: '1 hr',
                     level: 'Advanced',
-                    abstract: 'In this session you will learn all about directives!'
+                    abstract: 'In this session you will learn all about directives!',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
                     duration: '30 mins',
                     level: 'Beginner',
-                    abstract: 'A closer lok at scopes'                    
+                    abstract: 'A closer lok at scopes',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Well Behaved Controller',
                     creatorName: 'Jane Smith',
                     duration: '2 hours',
                     level: 'Intermediate',
-                    abstract: 'Controlers are your friends!'                    
+                    abstract: 'Controlers are your friends!',
+                    upVoteCount: 0
                 }
             ]
+        };
+
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
         };
     }
 );
